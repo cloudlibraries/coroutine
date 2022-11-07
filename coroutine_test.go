4ff6fbc965d605528297c9e83762ea07fe384c2e
lizongti <lzongti@gmail.com>
2022-11-07 14:21:11 +0800

    update go.mod

--- coroutine_test.go
+++ coroutine_test.go
@@ -3,59 +3,59 @@ package coroutine_test
 import (
 	"testing"
 
-	"github.com/cloudlibraries/coroutine"
-	"github.com/cloudlibraries/safe"
-	qt "github.com/frankban/quicktest"
+	"github.com/frankban/quicktest"
+	"github.com/golibraries/coroutine"
+	"github.com/golibraries/safe"
 )
 
 func TestCreate(t *testing.T) {
-	c := qt.New(t)
+	c := quicktest.New(t)
 
-	c.Run("Create", func(c *qt.C) {
+	c.Run("Create", func(c *quicktest.C) {
 		co, err := coroutine.Create(func(co *coroutine.Coroutine, args ...any) error {
 			output, err := co.Yield("Hello")
 			if err != nil {
 				return err
 			}
-			c.Assert(output, qt.DeepEquals, []any{"World"})
+			c.Assert(output, quicktest.DeepEquals, []any{"World"})
 
 			return nil
 		})
-		c.Assert(err, qt.IsNil)
+		c.Assert(err, quicktest.IsNil)
 
 		output, err := co.Resume()
-		c.Assert(err, qt.IsNil)
-		c.Assert(output, qt.DeepEquals, []any{})
+		c.Assert(err, quicktest.IsNil)
+		c.Assert(output, quicktest.DeepEquals, []any{})
 
 		output, err = co.Resume("World")
 
-		c.Assert(err, qt.IsNil)
-		c.Assert(output, qt.DeepEquals, "Hello")
+		c.Assert(err, quicktest.IsNil)
+		c.Assert(output, quicktest.DeepEquals, "Hello")
 	})
 }
 
 func TestStart(t *testing.T) {
-	c := qt.New(t)
+	c := quicktest.New(t)
 
-	c.Run("Start", func(c *qt.C) {
+	c.Run("Start", func(c *quicktest.C) {
 		err := coroutine.Start(func(co *coroutine.Coroutine, args ...any) error {
 			go safe.Do(func() error {
 				output, err := co.Yield("Hello")
 				if err != nil {
 					return err
 				}
-				c.Assert(output, qt.DeepEquals, []any{"World"})
+				c.Assert(output, quicktest.DeepEquals, []any{"World"})
 
 				return nil
 			})
 
 			output, err := co.Resume("World")
 
-			c.Assert(err, qt.IsNil)
-			c.Assert(output, qt.DeepEquals, "Hello")
+			c.Assert(err, quicktest.IsNil)
+			c.Assert(output, quicktest.DeepEquals, "Hello")
 			return nil
 		})
 
-		c.Assert(err, qt.IsNil)
+		c.Assert(err, quicktest.IsNil)
 	})
 }
--- go.mod
+++ go.mod
@@ -1,14 +1,14 @@
-module github.com/cloudlibraries/coroutine
+module github.com/golibraries/coroutine
 
 go 1.19
 
 require (
-	github.com/cloudlibraries/safe v1.0.1
 	github.com/frankban/quicktest v1.14.3
+	github.com/golibraries/safe v1.0.2
 )
 
 require (
-	github.com/cloudlibraries/cast v1.0.0 // indirect
+	github.com/golibraries/cast v1.0.1 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
