f98087112ede43a7532f23daa1291530c9e75e62
lizongti <lzongti@gmail.com>
2022-11-08 14:24:47 +0800

    update test

--- coroutine_test.go
+++ coroutine_test.go
@@ -3,59 +3,59 @@ package coroutine_test
 import (
 	"testing"
 
-	"github.com/frankban/quicktest"
+	. "github.com/frankban/quicktest"
 	"github.com/golibraries/coroutine"
 	"github.com/golibraries/safe"
 )
 
 func TestCreate(t *testing.T) {
-	c := quicktest.New(t)
+	c := New(t)
 
-	c.Run("Create", func(c *quicktest.C) {
+	c.Run("Create", func(c *C) {
 		co, err := coroutine.Create(func(co *coroutine.Coroutine, args ...any) error {
 			output, err := co.Yield("Hello")
 			if err != nil {
 				return err
 			}
-			c.Assert(output, quicktest.DeepEquals, []any{"World"})
+			c.Assert(output, DeepEquals, []any{"World"})
 
 			return nil
 		})
-		c.Assert(err, quicktest.IsNil)
+		c.Assert(err, IsNil)
 
 		output, err := co.Resume()
-		c.Assert(err, quicktest.IsNil)
-		c.Assert(output, quicktest.DeepEquals, []any{})
+		c.Assert(err, IsNil)
+		c.Assert(output, DeepEquals, []any{})
 
 		output, err = co.Resume("World")
 
-		c.Assert(err, quicktest.IsNil)
-		c.Assert(output, quicktest.DeepEquals, "Hello")
+		c.Assert(err, IsNil)
+		c.Assert(output, DeepEquals, "Hello")
 	})
 }
 
 func TestStart(t *testing.T) {
-	c := quicktest.New(t)
+	c := New(t)
 
-	c.Run("Start", func(c *quicktest.C) {
+	c.Run("Start", func(c *C) {
 		err := coroutine.Start(func(co *coroutine.Coroutine, args ...any) error {
 			go safe.Do(func() error {
 				output, err := co.Yield("Hello")
 				if err != nil {
 					return err
 				}
-				c.Assert(output, quicktest.DeepEquals, []any{"World"})
+				c.Assert(output, DeepEquals, []any{"World"})
 
 				return nil
 			})
 
 			output, err := co.Resume("World")
 
-			c.Assert(err, quicktest.IsNil)
-			c.Assert(output, quicktest.DeepEquals, "Hello")
+			c.Assert(err, IsNil)
+			c.Assert(output, DeepEquals, "Hello")
 			return nil
 		})
 
-		c.Assert(err, quicktest.IsNil)
+		c.Assert(err, IsNil)
 	})
 }
